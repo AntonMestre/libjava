6e7a3c7ed529a5d9395c409b64137921c268472f
antoninmaystre <anmaystre@gmail.com>
2024-09-21 22:24:39 +0200

    chore: adding javadoc

--- src/main/java/fr/antonincorp/Fakelib.java
+++ src/main/java/fr/antonincorp/Fakelib.java
@@ -1,7 +1,18 @@
 package fr.antonincorp;
 
+/**
+* Fakelib help faking stuff
+* 
+* @author Antonin
+*/
 public class Fakelib {
     
+    /**
+     * <p>
+     * getFakeString helps you getting a fakestring
+     * </p>
+     * @return a fake String
+     */
     public String getFakeString(){
         return "Fake";
     }
---
+++ target/reports/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"fr.antonincorp","c":"Fakelib","l":"Fakelib()","u":"%3Cinit%3E()"},{"p":"fr.antonincorp","c":"Fakelib","l":"getFakeString()"}];updateSearchResults();
